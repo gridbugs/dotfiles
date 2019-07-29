1682d622c23fd06ce5b6e8349f1513ce6bd94141
Stephen Sherratt <stephen@sherra.tt>
2019-07-29 13:05:33 +0100

    Default terminal is st

--- dwm/config.h
+++ dwm/config.h
@@ -57,7 +57,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_purple, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "termite", NULL };
+static const char *termcmd[]  = { "st", NULL };
 static const char *browsercmd[]  = { "firefox", NULL };
 
 static const char *monbrightnessinccmd[] = { "xbacklight", "-inc", "1", NULL };
