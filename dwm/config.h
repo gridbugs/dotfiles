cc25668fcd5227adc0aa426093919ece365bae7d
Stephen Sherratt <stephen@sherra.tt>
2019-07-29 07:59:54 +0100

    Set dwm window snap to 0

--- dwm/config.h
+++ dwm/config.h
@@ -2,7 +2,7 @@
 
 /* appearance */
 static const unsigned int borderpx  = 4;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int snap      = 0;        /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "xos4 Terminus:pixelsize=16:antialias=true:autohint=true" };
