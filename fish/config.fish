64e62e4e15321bb71266e94bfe829ce2041ba693
Stephen Sherratt <stephen@sherra.tt>
2019-08-25 13:12:00 +0100

    Add gls alias for macos

--- fish/config.fish
+++ fish/config.fish
@@ -7,7 +7,11 @@ alias vim nvim
 alias vmi nvim
 alias explicit-non-default-packages "bash -c 'comm -23 <(pacman -Qqe | sort) <(pacman -Qqg base base-devel | sort)'"
 
-alias ls 'ls --color --human-readable --group-directories-first'
+if type gls ^/dev/null >/dev/null
+    alias ls 'gls --color --human-readable --group-directories-first'
+else
+    alias ls 'ls --color --human-readable --group-directories-first'
+end
 
 set --export LS_COLORS "di=1;34:ln=35:so=32:pi=33:ex=31:bd=34;46:cd=34;43:su=30;41:sg=30;46:tw=1;30;42:ow=1;30;43"
 
