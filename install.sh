1063ae18aa930be0fa158ca2f0602c487ba42e1a
Stephen Sherratt <stephen@sherra.tt>
2020-02-06 21:02:10 +1100

    Remove redundant fzf clone and reorder

--- install.sh
+++ install.sh
@@ -19,17 +19,6 @@ cd "$HOME"
 make_link nvim .vim
 make_link nvim .config/nvim
 make_link nvim/init.vim .vimrc
-
-if [ ! -e "$HOME/.fzf" ]; then
-    git clone --depth 1 https://github.com/junegunn/fzf.git "$HOME/.fzf"
-fi
-
-# link archlinux-specific packages
-if type pacman 2>/dev/null >/dev/null; then
-    make_link dwm pkg/dwm
-    make_link st pkg/st
-fi
-
 make_link bashrc .bashrc
 make_link bash_profile .bash_profile
 make_link inputrc .inputrc
@@ -37,5 +26,11 @@ make_link xinitrc .xinitrc
 make_link tmux.conf .tmux.conf
 make_link htoprc .config/htop/htoprc
 make_link bin .bin
+
+# link archlinux-specific packages
+if type pacman 2>/dev/null >/dev/null; then
+    make_link dwm pkg/dwm
+    make_link st pkg/st
+fi
 
 echo Done!
