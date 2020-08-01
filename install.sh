86932a0a0c512f4f0731949d4843225520481375
Stephen Sherratt <stephen@sherra.tt>
2020-08-02 03:04:01 +1000

    Remove unused nixpkgs

--- install.sh
+++ install.sh
@@ -28,8 +28,6 @@ make_link tmux.conf .tmux.conf
 make_link bin .bin
 make_link hushlogin .hushlogin
 make_link git-prompt.sh .git-prompt.sh
-make_link dwm .config/nixpkgs/overlays/dwm
-make_link st .config/nixpkgs/overlays/st
 
 # link archlinux-specific packages
 if type pacman 2>/dev/null >/dev/null; then
