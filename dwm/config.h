e91480019073f027b0de5ca9315fb8b4fa1c3cc9
Stephen Sherratt <steve@gh.st>
2021-09-16 16:02:08 +1000

    Dwm shortcut for find-cursor

--- dwm/config.h
+++ dwm/config.h
@@ -67,6 +67,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_histogram", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_purple, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *browsercmd[]  = { "firefox", NULL };
+static const char *findcursorcmd[]  = { "find-cursor", "-f", "-c#aa33aa", "-l10", "-s500", "-w200", NULL };
 
 static const char *monbrightnessinccmd[] = { "setbacklight", "+10", NULL };
 static const char *monbrightnessdeccmd[] = { "setbacklight", "-10", NULL };
@@ -87,6 +88,7 @@ static Key keys[] = {
     { MODKEY,                       XK_r,      spawn,          {.v = dmenucmd } },
     { MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
     { MODKEY,                       XK_Return, spawn,          {.v = browsercmd } },
+    { MODKEY,                       XK_a,      spawn,          {.v = findcursorcmd } },
     { MODKEY,                       XK_t,      togglebar,      {0} },
     { MODKEY,                       XK_apostrophe,      focusstack,     {.i = +1 } },
     { MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
