184677b20efe4cf8dd8f512942fdd758720a019b
Stephen Sherratt <stephen@sherra.tt>
2022-10-31 20:03:46 +1100

    Allow unfree on u3

--- home-manager/u3/flake.nix
+++ home-manager/u3/flake.nix
@@ -13,7 +13,7 @@
   outputs = { nixpkgs, home-manager, ... }:
     let
       system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
+      pkgs = import nixpkgs { system = system; config = { allowUnfree = true; }; };
     in {
       homeConfigurations.s = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
