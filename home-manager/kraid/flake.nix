aae4ae2e0e0eb618140e742126501bb4daf5e14f
Stephen Sherratt <stephen@sherra.tt>
2022-11-01 21:44:03 +1100

    Add home-manager for kraid

---
+++ home-manager/kraid/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Home Manager configuration";
+
+  inputs = {
+    # Specify the source of Home Manager and Nixpkgs.
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { nixpkgs, home-manager, ... }:
+    let
+      system = "x86_64-darwin";
+      pkgs = import nixpkgs { system = system; config = { allowUnfree = true; }; };
+    in {
+      homeConfigurations.s = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+
+        # Specify your home configuration modules here, for example,
+        # the path to your home.nix.
+        modules = [ ./home.nix ];
+      };
+    };
+}
