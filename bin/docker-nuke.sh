3580fe8f6668f78afbe7a71485093c13da5893c8
Stephen Sherratt <stephen@sherra.tt>
2023-01-31 14:59:31 +1100

    Add docker-nuke.sh script

---
+++ bin/docker-nuke.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -e
+
+read -p "Are you sure? " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    set -x
+    docker container prune --force
+    docker image prune --force --all
+fi
