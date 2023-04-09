b78f2286ed21a9359f96d33e36dd431f74a9fa08
Stephen Sherratt <stephen@sherra.tt>
2023-04-10 01:00:11 +1000

    Add paarl home manager

---
+++ home-manager/paarl/home.nix
@@ -0,0 +1,37 @@
+{ config, pkgs, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "s";
+  home.homeDirectory = "/home/s";
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "22.05";
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  # direnv
+  programs.direnv.enable = true;
+  programs.direnv.nix-direnv.enable = true;
+
+  home.packages = with pkgs; [
+    firefox
+    neovim
+    unzip
+    projectm
+  ];
+
+  # Allows rust-analyzer to find the rust source
+  home.sessionVariables = {
+    RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
+  };
+}
