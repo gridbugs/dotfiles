bd9c36dc59316552df9dfd75623174d8ff3fbc55
Stephen Sherratt <stephen@sherra.tt>
2023-11-20 13:46:09 +1100

    Switch to stable packages for u3

--- home-manager/u3/flake.nix
+++ home-manager/u3/flake.nix
@@ -2,9 +2,9 @@
   description = "Home Manager configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-23.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
