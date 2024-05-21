40761176dd7088495da9996be07a83bce72a09a3
Stephen Sherratt <stephen@sherra.tt>
2024-05-21 17:37:16 +1000

    Disable ligatures in wezterm

--- wezterm.lua
+++ wezterm.lua
@@ -8,6 +8,7 @@ local config = wezterm.config_builder()
 
 config.color_scheme = 'Catppuccin Mocha'
 config.hide_tab_bar_if_only_one_tab = true
+config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
 
 -- and finally, return the configuration to wezterm
 return config
