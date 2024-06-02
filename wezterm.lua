a94a0e1a774eb0818d392edf8ad6630fb5bd6376
Stephen Sherratt <stephen@sherra.tt>
2024-06-02 16:30:19 +1000

    Wezterm bell config

--- wezterm.lua
+++ wezterm.lua
@@ -9,6 +9,16 @@ local config = wezterm.config_builder()
 config.color_scheme = 'Catppuccin Mocha'
 config.hide_tab_bar_if_only_one_tab = true
 config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
+config.audible_bell = 'Disabled'
+config.visual_bell = {
+  fade_in_function = 'EaseIn',
+  fade_in_duration_ms = 100,
+  fade_out_function = 'EaseOut',
+  fade_out_duration_ms = 100,
+}
+config.colors = {
+  visual_bell = '#2c2942',
+}
 
 -- and finally, return the configuration to wezterm
 return config
