62635f134ff3f6619e969d6cb088d5d3bb674696
Stephen Sherratt <stephen@sherra.tt>
2024-06-26 09:56:21 +1000

    Trivial: emacs

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -94,6 +94,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(default ((t (:background nil))))
  '(flycheck-error ((t (:background "red4" :underline nil))))
  '(flycheck-hint ((t (:background "green4" :underline nil))))
  '(flycheck-info ((t (:background "blue4" :underline nil))))
