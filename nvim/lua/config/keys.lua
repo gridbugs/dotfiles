f812fe745280b695cb750713dda92fb6ad9aeb31
Stephen Sherratt <stephen@sherra.tt>
2024-10-03 21:09:51 +1000

    Lsp shortcuts to nagivate to errors

--- nvim/lua/config/keys.lua
+++ nvim/lua/config/keys.lua
@@ -7,3 +7,7 @@ vim.cmd("au FileType ocaml map <leader>m :e %:p:s,.mli$,.X123X,:s,.ml$,.mli,:s,.
 
 -- open a fuzzy file searching window
 vim.keymap.set("n", "<leader>f", ":FzfLua files<CR>")
+
+-- navigate between errors reported by lsp
+vim.keymap.set('n', '<leader>dn', '<cmd>lua vim.diagnostic.goto_next()<CR>')
+vim.keymap.set('n', '<leader>dp', '<cmd>lua vim.diagnostic.goto_prev()<CR>')
