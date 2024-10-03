78ceb3d0911b200532f6f5f8356251680b455832
Stephen Sherratt <stephen@sherra.tt>
2024-10-03 15:58:14 +1000

    Tweak neotree shortcuts

--- nvim/lua/config/keys.lua
+++ nvim/lua/config/keys.lua
@@ -1,5 +1,6 @@
 -- open neotree in directory of current file or close it if it's already open
-vim.keymap.set("n", "<leader>d", ":Neotree toggle dir=%:p:h<CR>")
+vim.keymap.set("n", "<leader>d", ":Neotree dir=%:p:h<CR>")
+vim.keymap.set("n", "<leader>D", ":Neotree toggle dir=%:p:h<CR>")
 
 -- toggle between .ml and .mli file when editing ocaml
 vim.cmd("au FileType ocaml map <leader>m :e %:p:s,.mli$,.X123X,:s,.ml$,.mli,:s,.X123X$,.ml,<CR>")
