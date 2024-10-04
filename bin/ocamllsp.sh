c8ecf268e2d8a2e3930b50da59ada0889d5616fa
Stephen Sherratt <stephen@sherra.tt>
2024-10-04 20:00:55 +1000

    Fix ocamllsp script

--- bin/ocamllsp.sh
+++ bin/ocamllsp.sh
@@ -8,15 +8,19 @@ set -eu
 
 if [ -d dune.lock ]; then
 
-    # Make sure that the ocamlformat dev tool is installed as it's needed by
-    # ocamllsp. There's currently no command that just installs ocamlformat so
-    # we need to run `dune fmt` and ignore the result.
-    echo "Installing ocamlformat..."
-    dune fmt --preview > /dev/null 2> /dev/null || true  # the first build will fail because of https://github.com/ocaml/dune/issues/10903
-    dune fmt --preview > /dev/null
+    OCAMLFORMAT_TARGET=_build/_private/default/.dev-tool/ocamlformat/ocamlformat/target
+
+    if [ ! -f $OCAMLFORMAT_TARGET/cookie ]; then
+        # Make sure that the ocamlformat dev tool is installed as it's needed by
+        # ocamllsp. There's currently no command that just installs ocamlformat so
+        # we need to run `dune fmt` and ignore the result.
+        echo "Installing ocamlformat..."
+        dune fmt --preview > /dev/null 2> /dev/null || true  # the first build will fail because of https://github.com/ocaml/dune/issues/10903
+        dune fmt --preview > /dev/null
+    fi
 
     # Add ocamlformat to the environment in which ocamllsp runs so ocamllsp can invoke ocamlformat.
-    export PATH=$PWD/_build/_private/default/.dev-tool/ocamlformat/ocamlformat/target/bin:$PATH
+    export PATH=$PWD/$OCAMLFORMAT_TARGET/bin:$PATH
 
     # Build and run ocamllsp.
     dune tools exec ocamllsp $@
