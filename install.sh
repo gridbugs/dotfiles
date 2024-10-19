5eb0b4c54b8d53a3fe49f968b9f2af8c483165fe
Stephen Sherratt <stephen@sherra.tt>
2024-10-20 01:57:59 +1100

    Add back cofig.nix

--- install.sh
+++ install.sh
@@ -43,6 +43,7 @@ make_link emacs.d/init.el .emacs.d/init.el
 make_link emacs.d/cram-mode.el .emacs.d/cram-mode.el
 make_link emacs.d/flycheck-okra.el .emacs.d/flycheck-okra.el
 make_link wezterm.lua .wezterm.lua
+make_link nix.conf .config/nix/nix.conf
 
 # link archlinux-specific packages
 if type pacman 2>/dev/null >/dev/null; then
