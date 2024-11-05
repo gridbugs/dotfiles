85887bd98ab211ecd6ed7885b644b28a6d7676c6
Stephen Sherratt <stephen@sherra.tt>
2024-11-05 12:46:51 +1100

    Update ocamllsp wrapper

--- bin/ocamllsp.sh
+++ bin/ocamllsp.sh
@@ -13,19 +13,46 @@ if [ -d dune.lock ]; then
     if [ ! -f $OCAMLFORMAT_TARGET/cookie ]; then
         # Make sure that the ocamlformat dev tool is installed as it's needed by
         # ocamllsp. There's currently no command that just installs ocamlformat so
-        # we need to run `dune fmt` and ignore the result.
-        echo "Installing ocamlformat..."
-        dune fmt --preview > /dev/null 2> /dev/null || true  # the first build will fail because of https://github.com/ocaml/dune/issues/10903
-        dune fmt --preview > /dev/null
+        # we need to run it and ignore the result.
+        dune tools exec ocamlformat -- --help > /dev/null
     fi
 
     # Add ocamlformat to the environment in which ocamllsp runs so ocamllsp can invoke ocamlformat.
     export PATH=$PWD/$OCAMLFORMAT_TARGET/bin:$PATH
 
     # Build and run ocamllsp.
-    dune tools exec ocamllsp $@
+    dune tools exec ocamllsp -- $@
 else
 
-    # Fall back to an opam installation of ocamllsp if there is no lockdir.
-    opam exec ocamllsp $@
+    # Fallback behaviour for the case where we're not in a dune project with a
+    # lockdir. This script includes fallback behaviour so that the same editor
+    # configuration can be used to edit ocaml projects that use dune package
+    # management as well as ocaml projects that do not.
+
+    script_dir=$(dirname $(realpath $0))
+
+    # Remove the directory containing this script from PATH so it doesn't call itself
+    export PATH=$(echo $PATH | tr ':' '\n' | grep -v "^$script_dir$" | paste -sd ':')
+
+    if type opam 2> /dev/null > /dev/null; then
+
+        # If opam is installed then use it to run ocamllsp from the current
+        # opam switch. Note that if the user runs `eval $(opam env)` then the
+        # bin path from the current opam switch will be in PATH before the
+        # directory containing this wrapper script. Therefore the only way that
+        # this script can be invoked is if `eval $(opam env)` hasn't been run
+        # in the current shell, or ocaml-lsp-server isn't installed in the
+        # current opam switch. So in order to run the ocamllsp executable from
+        # the current opam switch (assuming it is installed) we have to run it
+        # with `opam exec`.
+        opam exec ocamllsp -- $@
+    else
+
+        # Fall back to running ocamllsp from PATH. This requires ocamllsp to be
+        # installed globally which is not recommended because ocamllsp must be
+        # compiled with the same version of the compiler as is used to compile
+        # the code that it's analyzing. Still there's a chance it could work so
+        # try it as a last resort.
+        ocamllsp $@
+    fi
 fi
