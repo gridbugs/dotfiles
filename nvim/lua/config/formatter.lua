ddbf6fd0e8532c6b89017c29fdaff4d97d990f9d
Stephen Sherratt <stephen@sherra.tt>
2024-11-08 15:43:13 +1100

    Less logs of formatter

--- nvim/lua/config/formatter.lua
+++ nvim/lua/config/formatter.lua
@@ -3,7 +3,7 @@ require("formatter").setup {
   -- Enable or disable logging
   logging = true,
   -- Set the log level
-  log_level = vim.log.levels.DEBUG,
+  log_level = vim.log.levels.WARN,
   -- All formatter configurations are opt-in
   filetype = {
     dune = {
