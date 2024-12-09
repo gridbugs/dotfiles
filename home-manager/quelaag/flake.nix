0e322a24853453acfb7462b5347363e3c97351ce
Stephen Sherratt <stephen@sherra.tt>
2024-12-09 13:51:45 +1100

    Update quelaag

--- home-manager/quelaag/flake.nix
+++ home-manager/quelaag/flake.nix
@@ -3,9 +3,9 @@
 
   inputs = {
     # Specify the source of Home Manager and Nixpkgs.
-    nixpkgs = { url = "github:nixos/nixpkgs/nixos-24.05"; };
+    nixpkgs = { url = "github:nixos/nixpkgs/nixos-24.11"; };
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.05";
+      url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
