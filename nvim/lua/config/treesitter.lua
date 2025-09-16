f46c2666ae974f5aa749f915460baa7f2eb85078
Stephen Sherratt <stephen@sherra.tt>
2025-09-16 12:21:09 +1000

    Enable ts syntax highlighting in neovim

--- nvim/lua/config/treesitter.lua
+++ nvim/lua/config/treesitter.lua
@@ -2,6 +2,8 @@ require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the listed parsers MUST always be installed)
   ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
 
+  highlight = { enable = true },
+
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
 
