e590b6ec474ebbe15e89543fc56499074fe4bc60
Stephen Sherratt <stephen@sherra.tt>
2026-04-28 14:29:56 +1000

    Fixes for digit blocks highlighting

--- nvim/lua/extra/digit_blocks.lua
+++ nvim/lua/extra/digit_blocks.lua
@@ -2,6 +2,11 @@ local digit_block_ns = vim.api.nvim_create_namespace("digit_blocks")
 vim.api.nvim_set_hl(0, "DigitBlock", { fg = "#FFFFFF" })
 
 local function highlight_digit_blocks_in_number(buf, line_index, number_start, number_end, block_size)
+    local syn_id = vim.fn.synID(line_index, number_start, true)
+    local name = vim.fn.synIDattr(vim.fn.synIDtrans(syn_id), 'name')
+    if name:lower():find("comment") ~= nil then
+      return
+    end
     local num_digits = number_end - number_start
     local num_blocks = ((num_digits - 1) / block_size) + 1
     for block_index = 1, num_blocks do
@@ -10,8 +15,6 @@ local function highlight_digit_blocks_in_number(buf, line_index, number_start, n
         local hl_group
         if block_index % 2 == 0 then
             hl_group = "DigitBlock"
-        else
-            hl_group = "Normal"
         end
         vim.api.nvim_buf_set_extmark(buf, digit_block_ns, line_index - 1, block_start, {
             end_col = block_end,
@@ -26,18 +29,6 @@ local function highlight_digit_blocks(buf)
     for line_index, line in ipairs(lines) do
         for hex_start, hex_end in line:gmatch("0x()%x+()") do
             highlight_digit_blocks_in_number(buf, line_index, hex_start, hex_end, 4)
-        end
-        for dec_start, dec_end in line:gmatch("()%d+()") do
-            local skip = false
-            if dec_start > 2 then
-                local prefix = line:sub(dec_start - 2, dec_start - 1)
-                if prefix:match('0[xXoObB]') then
-                    skip = true
-                end
-            end
-            if not skip then
-                highlight_digit_blocks_in_number(buf, line_index, dec_start, dec_end, 3)
-            end
         end
     end
 end
