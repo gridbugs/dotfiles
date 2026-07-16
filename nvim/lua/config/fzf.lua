64f465ca97223927cc58b797b48dc7742ff564f3
Stephen Sherratt <inconstevable@gmail.com>
2026-07-16 10:55:54 +1000

    Fzf rg fix

--- nvim/lua/config/fzf.lua
+++ nvim/lua/config/fzf.lua
@@ -3,6 +3,6 @@ require("fzf-lua").setup({
     fd_opts = [[--color=never --type f --type l --exclude .git --hidden --no-ignore --follow]],
   },
   grep = {
-    rg_opts = [[--column --line-number --no-heading --color=always --smart-case --max-columns=4096 -e --hidden --no-ignore --follow]],
+    rg_opts = [[--column --line-number --no-heading --color=always --smart-case --max-columns=4096 --hidden --no-ignore --follow]],
   }
 })
